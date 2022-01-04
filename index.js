c82fac96cde33e0f6f6f32c08e7e3db0415adeac
devdidar <didarulazammahi@gmail.com>
2022-01-04 23:01:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9very.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("");
+      const courseCollection = database.collection("");
+     
+    } finally {
+      //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+  
+  app.get("/", (req, res) => {
+    res.send("Hello CourseLance Server!");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
